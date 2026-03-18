38d1bde3917de483403aeab6546f7d925d44cd1b
Subista Kunwar <subistakunwar@gmail.com>
2026-03-18 11:18:08 +0545

    js operator

---
+++ js-class-1/script.js
@@ -0,0 +1,19 @@
+var a = 5;
+var b = 10;
+var sum = a + b ;
+console.log('The remainder of a and b is:', sum);
+
+var x = 25;
+var y = 4;
+var rem = x % y;
+console.log('The remainder of x and y is:', rem);
+
+let a = 5;
+if (true) {
+  console.log(a);
+
+  a = 7;
+  console.log(a);
+}
+
+console.log(a);
---
+++ js-class-2/script.js
@@ -0,0 +1,38 @@
+// let a = 50;
+
+// if (a > 20) {
+//   alert("a is greater than 20");
+// } else {
+//   alert("a is smaller than 20");
+// }
+
+// let a = 50;
+
+// if (a > 20) {
+//   alert("the value of a " + a + " is greater than 20");
+// } else {
+//   alert("the value of a " + a + " is smaller than 20");
+// }
+// console.log(`The value of a is ${a} so a is greater than 20`);
+
+/*let b = 20;
+
+if (b < 20) {
+  alert(`the value of b is ${b} so b is greater than 20`);
+} else if (b < 5) {
+  alert(`the value of b is ${b} so b is smaller than 5`);
+} else {
+  alert(`the value of b is ${b} so b is equal to 20`);
+}
+*/
+/*let num= '5'
+if(num===5)
+    alert ('the number 5 is number')
+else if (num==5)
+    alert('the number 5 is string')
+else
+    alert('the number is not 5 ')
+*/
+let d=10;
+let msg =(d<5) ? 'the value of d is less than 5' : 'the value of d is greater or equal 5';
+ console.log (msg);
